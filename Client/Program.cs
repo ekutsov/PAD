05472b5bf90488b3aa3a2ede200018a81aee681d
Evgeny Kutsov <i@ekutsov.ru>
2022-08-21 23:35:37 +0300

    refactiring auth controller

--- Client/Program.cs
+++ Client/Program.cs
@@ -5,7 +5,6 @@ using Radzen;
 using PFA.Client;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 
-using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
@@ -21,7 +20,6 @@ builder.Services.AddHttpClient("PFA.ServerAPI")
     .ConfigureHttpClient(client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
     .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
-// Supply HttpClient instances that include access tokens when making requests to the server project.
 builder.Services.AddScoped(provider =>
 {
     var factory = provider.GetRequiredService<IHttpClientFactory>();
@@ -33,18 +31,8 @@ builder.Services.AddOidcAuthentication(options =>
     options.ProviderOptions.ClientId = "pfa-blazor-client";
     options.ProviderOptions.Authority = "https://localhost:44310/";
     options.ProviderOptions.ResponseType = "code";
-
-    // Note: response_mode=fragment is the best option for a SPA. Unfortunately, the Blazor WASM
-    // authentication stack is impacted by a bug that prevents it from correctly extracting
-    // authorization error responses (e.g error=access_denied responses) from the URL fragment.
-    // For more information about this bug, visit https://github.com/dotnet/aspnetcore/issues/28344.
-    //
     options.ProviderOptions.ResponseMode = "query";
     options.AuthenticationPaths.RemoteRegisterPath = "https://localhost:44310/Identity/Account/Register";
-    
-    // Add the "roles" (OpenIddictConstants.Scopes.Roles) scope and the "role" (OpenIddictConstants.Claims.Role) claim
-    // (the same ones used in the Startup class of the Server) in order for the roles to be validated.
-    // See the Counter component for an example of how to use the Authorize attribute with roles
     options.ProviderOptions.DefaultScopes.Add("roles");
     options.UserOptions.RoleClaim = "role";
 });
