d5abe218176ac7cc3da3529fc1132b936b9de935
Evgeny Kutsov <i@ekutsov.ru>
2022-08-30 01:28:34 +0300

    Add finance service

--- src/Services/Finance/Controllers/WeatherForecastController.cs
+++ src/Services/Finance/Controllers/WeatherForecastController.cs
@@ -1,9 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace PAD.ApiGateway.Controllers;
+namespace PFA.Finance.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/weather")]
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
@@ -16,5 +16,17 @@ public class WeatherForecastController : ControllerBase
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
     {
         _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
     }
 }
